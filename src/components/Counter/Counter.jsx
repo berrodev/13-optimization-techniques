04ebdb4e1aadfea99eb1033cfdd27081c83c90a3
Francisco Berroeta <fsberroeta@gmail.com>
2025-01-09 17:31:22 -0300

    refactor: enhance Counter component to track changes history and calculate current value dynamically

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -5,9 +5,11 @@ import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
+import CounterHistory from './CounterHistory.jsx';
 
 function isPrime(number) {
   log('Calculating if is prime number', 2, 'other');
+
   if (number <= 1) {
     return false;
   }
@@ -31,14 +33,22 @@ const Counter = memo(function Counter({ initialCount }) {
     [initialCount]
   );
 
-  const [counter, setCounter] = useState(initialCount);
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([initialCount]);
+
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange,
+    0
+  );
 
   const handleDecrement = useCallback(function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [-1, ...prevCounterChanges]);
   }, []);
 
   const handleIncrement = useCallback(function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [1, ...prevCounterChanges]);
   }, []);
 
   return (
@@ -51,12 +61,14 @@ const Counter = memo(function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges} />
     </section>
   );
 });
+
 export default Counter;
